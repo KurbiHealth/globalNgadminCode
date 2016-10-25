11482b6692e7c96ef7c9b9b6de4cb370bb6f50d7
Matt Eckman <matteckman@gmail.com>
2016-10-25 17:00:59 -0400

    latest

--- customFields/stamplay_file_upload/view.js
+++ customFields/stamplay_file_upload/view.js
@@ -2,5 +2,5 @@ export default {
     getReadWidget:   () => 'error: cannot display file field as readable',
     getLinkWidget:   () => 'error: cannot display file field as linkable',
     getFilterWidget: () => 'error: cannot display file field as filter',
-    getWriteWidget:  () => '<stamplay-file-upload field="::field" value="value" entity="::entity"></stamplay-file-upload>'
+    getWriteWidget:  () => '<stamplay-file-upload field="::field" value="value" entity="::entity" entry="entry"></stamplay-file-upload>'
 };
