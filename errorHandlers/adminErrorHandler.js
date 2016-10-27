8cab2b9e6fbbb6ae68057796f2cbb3301ea6568f
Matt Eckman <matteckman@gmail.com>
2016-10-27 11:24:21 -0400

    latest

--- errorHandlers/adminErrorHandler.js
+++ errorHandlers/adminErrorHandler.js
@@ -1,10 +1,10 @@
 module.exports = function(admin) {
 
     // Experimental Error Handler
-    function appErrorHandler(response) {
+    function appErrorHandler(response,notification) {
 	
 		console.log('in globalNgadminCode/errorHandlers/adminErrorHandler.js, response',response);
-        
+        notification.log('error');
         return 'Global ADMIN error: ' + response.status + '(' + response.data + ')';
     
     }
