99007ae6995ee1fa44b9d209f96e6716b3c4dd3d
Matt Eckman <matteckman@gmail.com>
2016-10-27 11:34:33 -0400

    latest

--- errorHandlers/adminErrorHandler.js
+++ errorHandlers/adminErrorHandler.js
@@ -1,11 +1,23 @@
-module.exports = function(admin) {
+module.exports = function(admin,notification) {
 
     // Experimental Error Handler
-    function appErrorHandler(response,notification) {
-	
-		console.log('in globalNgadminCode/errorHandlers/adminErrorHandler.js, response',response);
-        notification.log('error');
-        return 'Global ADMIN error: ' + response.status + '(' + response.data + ')';
+    function appErrorHandler(response) {
+
+        if(response.error){
+        	var errorMessage = response.error.message;
+        	var errorStatus = response.error.status
+        }else if(response.data.error){
+        	var errorMessage = response.data.error.message;
+        	var errorStatus = response.data.error.status;
+        }else{
+        	var errorMessage = 'Unable to process.';
+        	var errorStatus = 'Status unknown';
+        }
+
+        console.log('Error: ' + errorStatus + ', ' + errorMessage);
+        //notification.log('Error: ' + errorStatus + ', ' + errorMessage);
+
+        return 'Global ADMIN error: ' + errorStatus + '(' + errorMessage + ')';
     
     }
     
