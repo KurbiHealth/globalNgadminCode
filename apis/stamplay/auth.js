5c9b4dd26cb94dcbe4fc849985c5df8b6bf63916
Matt Eckman <matteckman@gmail.com>
2016-10-29 21:11:07 -0400

    latest

--- apis/stamplay/auth.js
+++ apis/stamplay/auth.js
@@ -1,16 +1,24 @@
-module.exports = function(RestangularProvider) {
+module.exports = function(myApp) {
 
-    var token = window.localStorage.getItem("http://kpadmin-jwt");
-    if(typeof token == 'object' && token == null){
-        token = '';
-    }else{
-        token = token.replace(/"/g,'');
-        token = token.toString();
-    }
+	myApp.config(function(RestangularProvider){
+		
+		// http://kpadmin-jwt
+	    var url = location.url;
+console.log('url',url);
+	    var token = window.localStorage.getItem(url + "-jwt");
+	    
+	    if(typeof token == 'object' && token == null){
+	        token = '';
+	    }else{
+	        token = token.replace(/"/g,'');
+	        token = token.toString();
+	    }
 
-    RestangularProvider.setDefaultHeaders({
-        "Content-Type": 'application/json; charset=utf-8',
-        "x-stamplay-jwt": token
-    });
+	    RestangularProvider.setDefaultHeaders({
+	        "Content-Type": 'application/json; charset=utf-8',
+	        "x-stamplay-jwt": token
+	    });
+
+	})
 
 }
